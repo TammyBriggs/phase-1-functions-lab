511554c75862fab40df7cb78b7d6b2bde13f417a
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-03-20 15:12:59 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    let result;
+    if (streetNumber >= 42) {
+        result = streetNumber - 42;
+    } else {
+        result = 42 - streetNumber;
+    }
+    return result;
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    let blocks = distanceFromHqInBlocks(streetNumber);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, destinationBlock) {
+    let distance;
+    if (destinationBlock >= startingBlock) {
+        distance = destinationBlock - startingBlock;
+    } else {
+        distance = startingBlock - destinationBlock;
+    }
+    return distance * 264;
+}
+
+function calculatesFarePrice(startingBlock, destinationBlock) {
+    let d = distanceTravelledInFeet(startingBlock, destinationBlock)
+    if (d <= 400) {
+        return 0
+    }
+    else if (d > 400 && d <= 2000) {
+        return (d - 400) * 0.02;
+    }
+    else if (d > 2000 && d <= 2500)
+        return 25;
+    else {
+        return "cannot travel that far"
+    }
+}
